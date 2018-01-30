aecf08a2da6c2861cdd34f3dc38fdb8fc7daaf5f
Компания МРобот <mrobotby@users.noreply.github.com>
2018-01-30 10:45:01 +0300

    Add files via upload

---
+++ RoboRover -M1-Advanced(Education)-05/RoboRover-M1-ADV-Examples-Arduino-1.0/RR-M1-05-ADV-DIST-SENSORS-1-stable/RR-M1-05-ADV-DIST-SENSORS-1-stable.ino
@@ -0,0 +1,43 @@
+#include <ServoRoboRoverM1_05.h>
+#include <SharpA21RoboRoverM1_05.h>
+#include <HCSR04RoboRoverM1_05.h>
+
+ServoMotor myServo;
+Sharp mySharpA0;
+Sharp mySharpA1;
+Ultrasonic myUltrasonic;
+
+void setup() {
+  Serial.begin(9600);
+  myServo.attach();
+  mySharpA0.attach(A0);
+  mySharpA1.attach(A1);
+  myUltrasonic.attach();
+}
+
+void loop() {
+  // Программа по выводу значений с ИК-датчиков расстояния в Монитор последовательного порта Arduino IDE.
+
+  // Введение:
+  // Для работы программы не требуется подавать питание на моторы и сервопривод через Тумблеры питания.
+
+  // 1. Подключи робота к компьютеру и загрузи программу в робота через Arduino IDE.
+  // 2. Открой Монитор последовательного порта в Arduino IDE и смотри показания с датчика.
+
+  // Описание программы:
+  // Настройка -В программе настраивается Последовательной порт Serial на скорости 9600 бод/с. Настраивается сервопривод робота.
+
+  // Цикл -В Монитор последовательного порта выводятся показания с левого ИК-датчика, а затем с правого ИК-датчика. Потом происходит задержка в 1 секунду и цикл повторяется вновь.
+  "Справка о программе. Кликни на знак ?";
+
+  myServo.write(90);
+  Serial.print("Расстояние с левого ИК-датчика ");
+  Serial.println(mySharpA0.read());
+  Serial.print("Расстояние с правого ИК-датчика ");
+  Serial.println(mySharpA1.read());
+  Serial.print("Расстояние с УЗ-датчика ");
+  Serial.println(myUltrasonic.read());
+  Serial.println("");
+  delay(1000);
+
+}
---
+++ RoboRover -M1-Advanced(Education)-05/RoboRover-M1-ADV-Examples-Arduino-1.0/RR-M1-05-ADV-REMOTE-CONTROL-1-stable/RR-M1-05-ADV-REMOTE-CONTROL-1-stable.ino
@@ -0,0 +1,68 @@
+#include <MotorL298NRoboRoverM1_05.h>
+#include <IRremote.h>
+#include <ServoRoboRoverM1_05.h>
+#include <Servo.h>
+
+ServoMotor myServo;
+Motors myMotors;
+int RECV_PIN = 12;
+
+IRrecv irrecv(RECV_PIN);
+decode_results results;
+
+void setup()
+{
+  Serial.begin(9600);
+  irrecv.enableIRIn(); // Start the receiver
+  myMotors.attach();
+  myServo.attach();
+}
+
+void loop() {
+
+  // Программа по управлению роботом при помощи ИК-пульта. Не забудь достать защитную пленку из пульта! Батарейка внутри пульта.
+
+  // Введение:
+
+  // 1. Подключи робота к компьютеру и загрузи программу в робота через Arduino IDE.
+  // 2. Поставь два переключателя в крайнее левое положение.
+  // 3. Управляй роботом стрелочками, ОК-стоп. 1-сервопривод влево, 2-сервопривод центральное положение, 3-сервопривод вправо. Если на роботе установлен зеленый кожух, то он снижает уровень приема сигнала от пульта. Рекомендуется управлять роботом без кожуха.
+
+  if (irrecv.decode(&results))
+  {
+    Serial.println(results.value);
+    if (results.value == 16718055) //прямо
+    {
+      myMotors.write(255, 255);
+    }
+    if (results.value == 16726215) //стоп
+    {
+      myMotors.write(0, 0);
+    }
+    if (results.value == 16730805) //назад
+    {
+      myMotors.write(-255, -255);
+    }
+    if (results.value == 16716015) //влево
+    {
+      myMotors.write(-255, 255);
+    }
+    if (results.value == 16734885) //вправо
+    {
+      myMotors.write(255, -255);
+    }
+    if (results.value == 16736925) //серво центр
+    {
+      myServo.write(90);
+    }
+    if (results.value == 16753245) //серво влево
+    {
+      myServo.write(145);
+    }
+    if (results.value == 16769565) //серво вправо
+    {
+      myServo.write(35);
+    }
+    irrecv.resume();
+  }
+}
